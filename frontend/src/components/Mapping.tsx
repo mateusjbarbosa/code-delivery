da86a5ce86f2a81d1292044d29bc7a99e0527565
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-04-07 11:27:04 -0300

    feat: add reactive routes array and fetch api

--- frontend/src/components/Mapping.tsx
+++ frontend/src/components/Mapping.tsx
@@ -1,10 +1,22 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 
 import { Button, Grid, MenuItem, Select } from "@material-ui/core";
+
+import { Route } from "../util/models";
+
+const API_URL = process.env.API_URL;
 
 type Props = {};
 
 export const Mapping = (props: Props) => {
+  const [routes, setRoutes] = useState<Route[]>([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/routes`)
+      .then((data) => data.json())
+      .then((routes) => setRoutes(routes));
+  }, []);
+
   return (
     <Grid container>
       <Grid item sm={4} xs={12}>
@@ -13,11 +25,20 @@ export const Mapping = (props: Props) => {
             <MenuItem value="">
               <em>Selecione uma corrida</em>
             </MenuItem>
+            {routes.map((route, key) => (
+              <MenuItem key={key} value={route.id}>
+                {route.title}
+              </MenuItem>
+            ))}
           </Select>
           <Button fullWidth type="submit" color="primary" variant="contained">
             Iniciar corrida
           </Button>
         </form>
+      </Grid>
+
+      <Grid item sm={8} xs={12}>
+        Map
       </Grid>
     </Grid>
   );
